4147f760ed0631ccd3d3ef0b1e1437b92b2a0fc4
emrekizil <emre.kizil@ogr.iuc.edu.tr>
2025-05-06 22:12:10 +0300

    removed unused class, func

--- app/src/main/java/com/emrekizil/spacewatchapp/ui/AppState.kt
+++ app/src/main/java/com/emrekizil/spacewatchapp/ui/AppState.kt
@@ -4,9 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 
@@ -30,7 +28,4 @@ fun rememberAppState(
 class AppState(
     val navController: NavHostController,
     val coroutineScope: CoroutineScope
-) {
-    val currentDestination: NavDestination?
-        @Composable get() = navController.currentBackStackEntryAsState().value?.destination
-}
+)
